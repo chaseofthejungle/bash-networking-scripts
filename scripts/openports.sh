37ae4b763ccf3364d12c03d405a3ad4dce81c477
Chase <127450469+chaseofthejungle@users.noreply.github.com>
2024-03-14 18:50:33 -0400

    Added escape sequence enabling (for if necessary).

--- scripts/openports.sh
+++ scripts/openports.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #This script lists out open ports on a host device. The port range below (1-1023) can be adjusted as desired.
 
-echo "Enter Target Hostname or IP Address:"
+echo 'Enter Target Hostname or IP Address:'
 read target_ip
 
-echo "\n Open Ports discovered on $target_ip :"
+echo -e '\n Open Ports discovered on $target_ip :'
 nc -zv $target_ip 1-1023 2>&1 | grep succeeded
