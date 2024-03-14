d1c1db310f60e43157d2fe19f73963f21b2ad3ec
Chase <127450469+chaseofthejungle@users.noreply.github.com>
2024-03-14 18:49:44 -0400

    Added escape sequence enabling (for if necessary).

--- scripts/useractivity.sh
+++ scripts/useractivity.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # This script lists out activity of a user account.
 
-echo "\n User Account Activity"
-echo "Enter a Username:"
+echo -e 'User Account Activity'
+echo 'Enter a Username:'
 read username
 
-echo "\n Recent Login Activity:"
+echo -e '\nRecent Login Activity:'
 last | grep $username
 
-echo "\n User Home Directory Modifications:"
+echo -e '\nUser Home Directory Modifications:'
 auditctl -w /home/$username
 
-echo "\n Sudo Command Requests:"
+echo -e '\nSudo Command Requests:'
 cat /var/log/auth.log | grep sudo | grep $username
